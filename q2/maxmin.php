96d7e1f77f7ce066793c65d42b597edc6847a1e4
AnkitPokhariya <60503071+AnkitPokhariya@users.noreply.github.com>
2020-03-22 19:08:12 +0530

    question 2
    assignment

---
+++ q2/maxmin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Parse JSON Data</title>
+	<script type="text/javascript" src="jsondata.js"></script>
+</head>
+<body>
+<p><strong><i>users that have completed maximum no. of tasks</i></strong></p>
+<p id="max"></p><br>
+<p><strong><i>users that have completed maximum no. of tasks</i></strong></p>
+<p id="min"></p>
+<script type="text/javascript" > 
+	document.getElementById("max").innerHTML = trueid;
+	document.getElementById("min").innerHTML = falseid;
+</script>
+</body>
+</html>
